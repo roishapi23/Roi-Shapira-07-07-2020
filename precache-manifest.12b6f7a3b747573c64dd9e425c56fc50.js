59a278edf1058c7cd9a56e7cf65f22b717204deb
roishapi23 <roishapi23@gmail.com>
2020-07-12 04:22:02 +0300

    Updates

---
+++ precache-manifest.12b6f7a3b747573c64dd9e425c56fc50.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dac51ec94fdb864676bc28e5c27a9ca9",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "4e32b7a5dc778aeb518b",
+    "url": "/weather/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "74c6d5cd8914ac00ff08",
+    "url": "/weather/static/css/main.00f97501.chunk.css"
+  },
+  {
+    "revision": "4e32b7a5dc778aeb518b",
+    "url": "/weather/static/js/2.575d6df5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/weather/static/js/2.575d6df5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74c6d5cd8914ac00ff08",
+    "url": "/weather/static/js/main.6b9d57eb.chunk.js"
+  },
+  {
+    "revision": "bdd4e2bf0ce8946d3851",
+    "url": "/weather/static/js/runtime-main.51874b67.js"
+  },
+  {
+    "revision": "39ac499c2a7856b24ffe3ffde4c8cc61",
+    "url": "/weather/static/media/15.39ac499c.png"
+  },
+  {
+    "revision": "0cf9cad1347244da8a9e197998e0eff9",
+    "url": "/weather/static/media/16.0cf9cad1.png"
+  },
+  {
+    "revision": "3c85da3e400a073c51b3ea73bdf5a130",
+    "url": "/weather/static/media/17.3c85da3e.png"
+  },
+  {
+    "revision": "da6fee650a05baae566fad32a3d2f0bd",
+    "url": "/weather/static/media/2.da6fee65.jpg"
+  },
+  {
+    "revision": "1ffb08a03346b57e26b885b84bc8a74a",
+    "url": "/weather/static/media/4.1ffb08a0.jpg"
+  },
+  {
+    "revision": "421c13f1b1eb0ae947bb64472fec6baa",
+    "url": "/weather/static/media/41.421c13f1.png"
+  }
+]);
